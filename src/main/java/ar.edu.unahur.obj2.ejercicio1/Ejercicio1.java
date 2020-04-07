6bc4a78f43fd9fc1754c0c5eae1b37ae4ca05f74
SkielAndy <ezequielandresm99@gmail.com>
2020-04-07 20:41:13 -0300

    ejercicio 1 prueba de push

--- src/main/java/ar.edu.unahur.obj2.ejercicio1/Ejercicio1.java
+++ src/main/java/ar.edu.unahur.obj2.ejercicio1/Ejercicio1.java
@@ -2,7 +2,18 @@ package ar.edu.unahur.obj2.ejercicio1;
 
 public class Ejercicio1 {
 
-    public int[] ordenar() {
-        return new int[] {};
+    public static void main(String[] args){
+        int[] v = {1,2,3,4,5,6,7,8,9,10};
+        System.out.println("Indice\tValor");
+        for(int i=0; i<v.length; i++)
+            System.out.println(i+"\t"+v[i]);
+        System.out.println();
+        System.out.println("Indice\tValor");
+        for(int i=v.length-1; i>=0; i--)
+            System.out.println(i+"\t"+v[i]);
     }
 }
+
+
+
+
